98f9941791cc5ea7a0d9a0df01c19be68f71bc3a
廖方正清 <liaofangzhengqing@xiaoduotech.com>
2020-10-31 16:10:08 +0800

    first commit

--- renderer.js
+++ renderer.js
@@ -4,3 +4,8 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const add = document.querySelector("#add");
+const ipc = require("electron").ipcRenderer;
+add.onclick = () => {
+  ipc.send("add");
+};
